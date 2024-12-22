806155a9d55bfe619fd6559c6cfe1a282d02a0e9
MarcVil7 <marcvilaespunya@gmail.com>
2024-12-22 16:47:30 +0100

    change endpoints to test

--- TestingApi/TestingApi/Program.cs
+++ TestingApi/TestingApi/Program.cs
@@ -27,24 +27,18 @@ public class Program
 
         app.UseAuthorization();
 
-        var summaries = new[]
+        app.MapGet("/test1", (HttpContext httpContext) =>
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-        {
-            var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                {
-                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    TemperatureC = Random.Shared.Next(-20, 55),
-                    Summary = summaries[Random.Shared.Next(summaries.Length)]
-                })
-                .ToArray();
-            return forecast;
+            return "test1";
         })
-        .WithName("GetWeatherForecast")
+        .WithName("test1")
+        .WithOpenApi();
+
+          app.MapGet("/test2", (HttpContext httpContext) =>
+        {
+            return "test2";
+        })
+        .WithName("test2")
         .WithOpenApi();
 
         app.Run();
